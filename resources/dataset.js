b6a6e298913c786a69eef2aa4a98970c5917ed0c
Lee Yeong Khang <yeong-khang.lee@vitrox.com>
2016-12-20 16:32:42 +0800

    detect datatype before send

--- resources/dataset.js
+++ resources/dataset.js
@@ -44,15 +44,46 @@ app.post('/dataset/upload', upload.single('file'), function (req, res) {
             fs.readFile(req.file.path, function (err, data) {
                 var newPath = serverPath + "/data/" + path + '/' + req.body.path
                 var newFullPath = newPath + '/' + req.file.originalname;
-                if (!fs.existsSync(newPath)){
-                    mkdirp.sync(newPath);
-                }
 
-                fs.writeFile(newFullPath, data, function (err) {
+                var mime = req.file.mimetype.split('/')[0];
+                if(mime == 'image'){
+                    if (!fs.existsSync(newPath)){
+                        mkdirp.sync(newPath);
+                    }
+
+                    fs.writeFile(newFullPath, data, function (err) {
+                        fs.unlink(req.file.path);
+                        res.status(200).json({"status": "done"});
+                    });
+                } else {
                     fs.unlink(req.file.path);
-                    res.status(200).json({"status": "done"});
-                });
+                    res.status(500).json({"error": "target is not image"});
+                }
             });
         });
     }
+});
+
+function walkSync(dir, filelist) {
+    var fs = fs || require('fs'),
+        files = fs.readdirSync(dir);
+    filelist = filelist || [];
+    files.forEach(function(file) {
+        if (fs.statSync(dir + file).isDirectory()) {
+            filelist = walkSync(dir + file + '/', filelist);
+        } else {
+            filelist.push({name: file, path: dir + '/' + file});
+        }
+    });
+    return filelist;
+};
+
+app.get('/dataset/:id', function (req, res) {
+    var stmt = db.prepare("SELECT * FROM datas WHERE id=?");
+    stmt.get(req.params.id, function(err, row){
+        stmt.finalize()
+        var path = row['path'];
+        var list = walkSync('./data/' + path + '/');
+        res.status(200).json({"data": list})
+    });
 });
